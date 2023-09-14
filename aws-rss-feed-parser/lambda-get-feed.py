c348455231f5328edacb3a5b36e1f8b50a131054
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-09-14 06:51:35 -0500

    adding DynamoDB
    updating serverlesss

--- aws-rss-feed-parser/lambda-get-feed.py
+++ aws-rss-feed-parser/lambda-get-feed.py
@@ -26,32 +26,32 @@ def get_feed_info(parsed):
     }
     return feed_info
 
-def get_articles(parsed):
-    articles = []
-    entries = parsed['entries']
+# def get_articles(parsed):
+#     articles = []
+#     entries = parsed['entries']
     
-    for entry in entries:
-        articles.append({
-            'id': entry['id'],
-            'link': entry['link'],
-            'published': entry['published_parsed'],
-            'title': entry['title'],
-            'author': entry['author_detail']['name'],
-            'summary': entry['summary']
-        })
+#     for entry in entries:
+#         articles.append({
+#             'id': entry['id'],
+#             'link': entry['link'],
+#             'published': entry['published_parsed'],
+#             'title': entry['title'],
+#             'author': entry['author_detail']['name'],
+#             'summary': entry['summary']
+#         })
         
-    return articles
+#     return articles
 
 def lambda_handler(event, context):
     parsed = feedparser.parse('https://cointelegraph.com/rss')
     feed_info = get_feed_info(parsed)
-    articles = get_articles(parsed)
+    #articles = get_articles(parsed)
     
     response = {
         "statusCode": 200,
         "body": json.dumps({
             "feed_info": feed_info,
-            "articles": articles
+            #"articles": articles
         })
     }
     
