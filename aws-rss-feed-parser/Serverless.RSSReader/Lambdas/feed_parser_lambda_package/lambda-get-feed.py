6788448667c3fe2e83ea79d38596b838ae56d025
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-09-14 10:54:36 -0500

    removing html tags

--- aws-rss-feed-parser/Serverless.RSSReader/Lambdas/feed_parser_lambda_package/lambda-get-feed.py
+++ aws-rss-feed-parser/Serverless.RSSReader/Lambdas/feed_parser_lambda_package/lambda-get-feed.py
@@ -1,5 +1,6 @@
 import json
 import feedparser
+import re
 
 def lambda_handler(event, context):
     parsed = feedparser.parse('https://cointelegraph.com/rss')
@@ -32,10 +33,15 @@ def get_feed_info_and_articles(parsed):
             'feed_article_published_date': entry['published_parsed'],
             'feed_article_title': entry['title'],
             'feed_article_author': entry['author_detail']['name'],
-            'feed_article_summary': entry['summary']
+            'feed_article_summary': remove_html_tags(entry['summary'])
         }
         # Merge the feed_info and article dictionaries
         article.update(feed_info)
         articles.append(article)
         
     return articles
+
+def remove_html_tags(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
