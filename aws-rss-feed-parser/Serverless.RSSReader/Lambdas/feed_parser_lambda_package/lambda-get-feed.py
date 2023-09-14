5e579ac7010d160ed54a43f70defcbd848fb9300
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-09-14 10:50:36 -0500

    fixed lambda
    fixed lambda to pull correct records from rss feed

--- aws-rss-feed-parser/Serverless.RSSReader/Lambdas/feed_parser_lambda_package/lambda-get-feed.py
+++ aws-rss-feed-parser/Serverless.RSSReader/Lambdas/feed_parser_lambda_package/lambda-get-feed.py
@@ -1,44 +1,41 @@
 import json
 import feedparser
 
-# Let's parse the RSS feed
 def lambda_handler(event, context):
     parsed = feedparser.parse('https://cointelegraph.com/rss')
-    feed_info = get_feed_info(parsed)
-    #articles = get_articles(parsed)
+    articles = get_feed_info_and_articles(parsed)
     
     response = {
         "statusCode": 200,
         "body": json.dumps({
-            "feed_info": feed_info,
-            #"articles": articles
+            "articles": articles
         })
     }
     
     return response
 
-def get_feed_info(parsed):
+def get_feed_info_and_articles(parsed):
     feed = parsed['feed']
     feed_info = {
-        'title': feed['title'],
-        'subtitle': feed['subtitle'],
-        'link': feed['link']
+        'rss_feed_title': feed['title'],
+        'rss_feed_subtitle': feed['subtitle'],
+        'rss_feed_link': feed['link']
     }
-    return feed_info
-
-# def get_articles(parsed):
-#     articles = []
-#     entries = parsed['entries']
     
-#     for entry in entries:
-#         articles.append({
-#             'id': entry['id'],
-#             'link': entry['link'],
-#             'published': entry['published_parsed'],
-#             'title': entry['title'],
-#             'author': entry['author_detail']['name'],
-#             'summary': entry['summary']
-#         })
+    articles = []
+    entries = parsed['entries']
+    
+    for entry in entries:
+        article = {
+            'feed_article_id': entry['id'],
+            'feed_article_link': entry['link'],
+            'feed_article_published_date': entry['published_parsed'],
+            'feed_article_title': entry['title'],
+            'feed_article_author': entry['author_detail']['name'],
+            'feed_article_summary': entry['summary']
+        }
+        # Merge the feed_info and article dictionaries
+        article.update(feed_info)
+        articles.append(article)
         
-#     return articles
-
+    return articles
