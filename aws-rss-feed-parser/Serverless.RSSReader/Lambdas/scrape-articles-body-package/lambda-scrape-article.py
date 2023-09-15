53f219992792eecd5e140a62848ead6ae197dee2
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-09-15 10:57:45 -0500

    updated response

--- aws-rss-feed-parser/Serverless.RSSReader/Lambdas/scrape-articles-body-package/lambda-scrape-article.py
+++ aws-rss-feed-parser/Serverless.RSSReader/Lambdas/scrape-articles-body-package/lambda-scrape-article.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     response = {
         "statusCode": 200,
         "body": json.dumps({
-            "articles": "Works"
+            "response": "Works"
         })
     }
     
