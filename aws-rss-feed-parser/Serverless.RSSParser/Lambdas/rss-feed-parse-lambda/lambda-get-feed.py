e123af1416daeb40f4e8bc28a672d401a69962eb
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-10-16 17:44:40 -0500

    updating rssfeed

--- aws-rss-feed-parser/Serverless.RSSParser/Lambdas/rss-feed-parse-lambda/lambda-get-feed.py
+++ aws-rss-feed-parser/Serverless.RSSParser/Lambdas/rss-feed-parse-lambda/lambda-get-feed.py
@@ -22,7 +22,7 @@ table_name = os.environ.get('DYNAMODB_TABLE_NAME')
 def lambda_handler(event, context):
     # parse the RSS feed from the list below
     #links = ['https://cointelegraph.com/rss', 'https://www.coindesk.com/arc/outboundfeeds/rss']
-    links = ['https://cointelegraph.com/rss']
+    links = ['https://cointelegraph.com/rss/tag/bitcoin']
     
     all_articles = []
     
