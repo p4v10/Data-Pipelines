9ecd8503e85d799800f33e1009c6fa513d1b3aa2
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-10-21 01:18:25 -0500

    selenium scraper

--- aws-rss-feed-parser/Serverless.RSSParser/Lambdas/rss-feed-parse-lambda/lambda-get-feed.py
+++ aws-rss-feed-parser/Serverless.RSSParser/Lambdas/rss-feed-parse-lambda/lambda-get-feed.py
@@ -19,39 +19,8 @@ dynamoDB = boto3.client('dynamodb')
 # define the DynamoDB table name
 table_name = os.environ.get('DYNAMODB_TABLE_NAME')
 
-def lambda_handler(event, context):
-    # parse the RSS feed from the list below
-    links = ['https://cointelegraph.com/rss/tag/bitcoin', 'https://cointelegraph.com/rss/tag/regulation']
-    
-    all_articles = []
-    
-    for link in links:
-        parsed = feedparser.parse(link)
-        
-        # extract feed information and articles from the parsed feed
-        articles = get_feed_info_and_articles(parsed)
-        all_articles.extend(articles)
-    
-    # write each article to DynamoDB
-    for article in all_articles:
-        # check if article already was written to the DynamoDB
-        if not article_exists(article['feed_article_id']):
-            article['unique_id'] = str(uuid.uuid4())  # Generate a unique ID
-            article['date_added'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current date and time
-            write_article_to_dynamoDB(article)
-    
-    # create a response dictionary with a 200 status code and JSON body
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({
-            "articles": all_articles
-        })
-    }
-    
-    return response
-
+# check if the article with the given ID exists in the DynamoDB table
 def article_exists(article_id):
-    # check if the article with the given ID exists in the DynamoDB table
     response = dynamoDB.query(
         TableName=table_name,
         KeyConditionExpression='feed_article_id = :id',
@@ -60,28 +29,6 @@ def article_exists(article_id):
         }
     )
     return len(response.get('Items', [])) > 0
-
-def write_article_to_dynamoDB(article):
-    try:
-        # write the article to the DynamoDB table
-        dynamoDB.put_item(
-            TableName=table_name,
-            Item={
-                'feed_article_id': {'S': article['feed_article_id']},
-                'feed_article_link': {'S': article['feed_article_link']},
-                'feed_article_published_date': {'S': article['feed_article_published_date']},
-                'feed_article_title': {'S': article['feed_article_title']},
-                'feed_article_author': {'S': article['feed_article_author']},
-                'feed_article_summary': {'S': article['feed_article_summary']},
-                'rss_feed_title': {'S': article['rss_feed_title']},
-                'rss_feed_subtitle': {'S': article['rss_feed_subtitle']},
-                'rss_feed_link': {'S': article['rss_feed_link']},
-                'date_added': {'S': article['date_added']},
-                'unique_id': {'S': article['unique_id']} 
-            }
-        )
-    except Exception as e:
-        logger.info(f"Error writing article to DynamoDB: {str(e)}")
 
 # Function that gets the Feed Information and Single article
 # It extracts all the necessary information needed to write the data into DynamoDB
@@ -126,7 +73,76 @@ def get_feed_info_and_articles(parsed):
 
     return articles
 
+# Writes articles to DynamoDB
+def write_article_to_dynamoDB(article):
+    try:
+        # write the article to the DynamoDB table
+        dynamoDB.put_item(
+            TableName=table_name,
+            Item={
+                'feed_article_id': {'S': article['feed_article_id']},
+                'feed_article_link': {'S': article['feed_article_link']},
+                'feed_article_published_date': {'S': article['feed_article_published_date']},
+                'feed_article_title': {'S': article['feed_article_title']},
+                'feed_article_author': {'S': article['feed_article_author']},
+                'feed_article_summary': {'S': article['feed_article_summary']},
+                'rss_feed_title': {'S': article['rss_feed_title']},
+                'rss_feed_subtitle': {'S': article['rss_feed_subtitle']},
+                'rss_feed_link': {'S': article['rss_feed_link']},
+                'date_added': {'S': article['date_added']},
+                'unique_id': {'S': article['unique_id']} 
+            }
+        )
+    except Exception as e:
+        logger.info(f"Error writing article to DynamoDB: {str(e)}")
+
 # Function to remove HTML tags from a given text
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
+
+# Remove some of the troublesome links that will require to build specific solutions in Selenium
+def filter_articles_by_link(articles, pattern):
+
+    if not isinstance(pattern, re.Pattern):
+        pattern = re.compile(pattern)
+
+    filtered_articles = [article for article in articles if not pattern.search(article['feed_article_link'])]
+
+    return filtered_articles
+
+def lambda_handler(event, context):
+    # parse the RSS feed from the list below
+    links = ['https://cointelegraph.com/rss/tag/bitcoin', 'https://cointelegraph.com/rss/tag/regulation']
+    
+    all_articles = []
+    
+    for link in links:
+        parsed = feedparser.parse(link)
+        
+        # extract feed information and articles from the parsed feed
+        articles = get_feed_info_and_articles(parsed)
+
+        # filter articles based on the link pattern
+        pattern = re.compile(r'\.com/news/')
+        articles = filter_articles_by_link(articles, pattern)
+
+        all_articles.extend(articles)
+    
+    # write each article to DynamoDB
+    for article in all_articles:
+        # check if article already was written to the DynamoDB
+        if not article_exists(article['feed_article_id']):
+            article['unique_id'] = str(uuid.uuid4())  # Generate a unique ID
+            article['date_added'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current date and time
+            write_article_to_dynamoDB(article)
+    
+    # create a response dictionary with a 200 status code and JSON body
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({
+            "articles": all_articles
+        })
+    }
+    
+    return response
