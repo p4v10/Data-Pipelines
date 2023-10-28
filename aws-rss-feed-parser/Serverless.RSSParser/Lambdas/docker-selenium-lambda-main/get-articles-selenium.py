fcc4089793885451b2c0ba91befb8b1b997e45e8
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-10-27 20:53:21 -0500

    scraping and updating dynamodb
    scraping articles, refining table and updating dynamodb records

--- aws-rss-feed-parser/Serverless.RSSParser/Lambdas/docker-selenium-lambda-main/get-articles-selenium.py
+++ aws-rss-feed-parser/Serverless.RSSParser/Lambdas/docker-selenium-lambda-main/get-articles-selenium.py
@@ -20,7 +20,36 @@ dynamoDB = boto3.client('dynamodb')
 # define the DynamoDB table name
 table_name = os.environ.get('DYNAMODB_TABLE_NAME')
 
-def lambda_handler(event=None, context=None):
+# get the news article from the RSS records in DynamoDB
+def get_dynamoDB_article_links():
+
+    links = []
+    # defining scan parameters to filter where article is not scraped yet
+    scan_kwargs = {
+        'FilterExpression': "article_scraped <> :one",
+        'ProjectionExpression': "feed_article_id, unique_id",
+        'ExpressionAttributeValues': {":one": {"N": "1"}}
+    }
+
+    done = False # indicates if all items were scraped
+    start_key = None # last scanned item, for pagination
+
+    while not done:
+        if start_key:
+            scan_kwargs['ExclusiveStartKey'] = start_key
+        # scna dynamodb with parameters
+        response = dynamoDB.scan(TableName = table_name, **scan_kwargs)
+        # append links and IDs to the list
+        links += [(item['feed_article_id']['S'], item['unique_id']['S']) 
+            for item in response.get('Items', []) 
+            if 'feed_article_id' in item and 'unique_id' in item]
+        start_key = response.get('LastEvaluatedKey', None)
+        done = start_key is None
+    logging.info(f'Links: {links}')
+    return links
+
+# scrape the full article text from the news website
+def get_full_article_text(url):
     # this block initialize chrome agent and sets the configurations
     options = webdriver.ChromeOptions()
     service = webdriver.ChromeService("/opt/chromedriver")
@@ -40,12 +69,12 @@ def lambda_handler(event=None, context=None):
     options.add_argument("--remote-debugging-port=9222")
 
     with webdriver.Chrome(options=options, service=service) as chrome:
-        chrome.get("https://cointelegraph.com/magazine/australia-145m-exchange-scandal-bitget-4th-china-lifts-nft-ban-asia-express/")
+        chrome.get(url)
 
         # scroll the page to mimic user
         for _ in range(5):
             chrome.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
+            time.sleep(5)
         
         # get full text from the article block
         article_element = chrome.find_element(By.CSS_SELECTOR, "article.post__article")
@@ -70,5 +99,43 @@ def lambda_handler(event=None, context=None):
         article_text = re.sub(r'Related.*?\.', '', article_text, flags=re.DOTALL)
         article_text = re.sub(r'(?<!\.)\n', '. ', article_text)
         article_text = article_text.replace('NEWS.', '').replace('Ad.', '').replace('\n', ' ')
-
+        logging.info(f'Article Text: {article_text}')
         return article_text
+
+# update the dynamoDB table with the scraped data
+def update_dynamoDB(feed_article_id, unique_id, article_text):
+    try:
+        dynamoDB.update_item(
+            TableName = table_name,
+            Key = {
+                'feed_article_id': {'S': feed_article_id},
+                'unique_id': {'S': unique_id}
+                },
+            UpdateExpression = "SET raw_full_article = :val1, article_scraped = :val2",
+            ExpressionAttributeValues = {
+                ':val1': {'S': article_text},
+                ':val2': {'N': '1'}
+            }
+        )
+        logging.info('DynamoDB updated')
+    except Exception as e:
+        logging.error(str(e))
+
+def lambda_handler(event, context):
+
+    # get links
+    links = get_dynamoDB_article_links()
+    processed_count = 0
+
+    for link, unique_id in links:
+        article_text = get_full_article_text(link)
+        update_dynamoDB(link, unique_id, article_text)
+        processed_count += 1
+        logging.info(f'Processed already: {processed_count}')
+
+    logging.info(f'Final Processed: {processed_count}')
+    return {
+        'statusCode': 200,
+        'body': f'Processed {processed_count} articles.'
+    }
+    
