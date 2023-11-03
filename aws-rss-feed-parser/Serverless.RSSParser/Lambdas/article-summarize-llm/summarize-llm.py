b44e42fc3c20d3c68d28ffbf9a3d6b8669094fea
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-11-03 00:32:27 -0500

    adding necessary resources

---
+++ aws-rss-feed-parser/Serverless.RSSParser/Lambdas/article-summarize-llm/summarize-llm.py
@@ -0,0 +1,14 @@
+import os
+import json
+import boto3
+import logging
+
+# initialize logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info("-----Start Loggining-----")
+
+def lambda_handler(event, context):
+
+    pass
